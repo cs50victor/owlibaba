69fa9deb6a00479ffc1e23e17d71ab0cb69f0200
Victor A <52110451+cs50victor@users.noreply.github.com>
2021-07-14 14:34:37 -0400

    activty signs

--- pages/complete.jsx
+++ pages/complete.jsx
@@ -17,10 +17,13 @@ export default function complete() {
       <div tw="max-w-screen-lg mx-auto text-center min-h-full pt-20 pb-36 px-5">
         <div tw="text-5xl md:text-5xl mb-20 font-bold">Your Order Is Processing </div>
         <div tw="relative w-80 h-72 mx-auto mb-10">
-          <Image src="/images/wow.gif" alt="" layout="fill" />
+          <Image src="/images/wow.gif" alt="Processing" layout="fill" tw="rounded-lg py-5" 
+            placeholder="blur"
+            blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
+            priority/>
         </div>
         <div tw="mb-8">
-          <span tw="w-full inline-flex max-w-md items-center justify-center">
+          <span tw="w-full inline-flex max-w-md items-center justify-center ">
             <Youtube tw="w-10 h-10 text-red-600" />
             {" -> "}
             <SparklesIcon tw="h-5 w-5 text-yellow-600" />
