186a88c66a04a22c42fadbaa7eb0a29a31581e59
Victor A <52110451+cs50victor@users.noreply.github.com>
2021-07-14 07:36:06 -0400

    minor bug

--- pages/muchWow.jsx
+++ pages/muchWow.jsx
@@ -10,7 +10,7 @@ export default function MuchWow() {
       <div tw="max-w-screen-lg mx-auto text-center min-h-full pt-20 pb-36 px-5">
         <div tw="text-5xl md:text-5xl mb-20 font-bold">Random Page</div>
         <div tw="relative w-72 h-72 mx-auto mb-10">
-          <Image src="/images/wow.gif" alt="" layout="fill"/>
+          <Image src="/images/wow.gif" alt="" layout="fill" />
         </div>
         <Link href="/" passHref>
           <Button as="a" tw="p-4 w-64 font-extrabold mx-auto">
